be91d66898fce173d4cd4d74fce863701741b870
paki1019 <paki1019@gmail.com>
2021-03-27 17:19:49 +0900

    프로젝트 생성

---
+++ src/main/java/jino/atelier/instagramimagescrapper/InstagramImageScrapperApplication.java
@@ -0,0 +1,13 @@
+package jino.atelier.instagramimagescrapper;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InstagramImageScrapperApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InstagramImageScrapperApplication.class, args);
+	}
+
+}
---
+++ src/test/java/jino/atelier/instagramimagescrapper/InstagramImageScrapperApplicationTests.java
@@ -0,0 +1,13 @@
+package jino.atelier.instagramimagescrapper;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InstagramImageScrapperApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
